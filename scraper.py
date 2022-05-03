7d333402282753d5725f2a55c2ed38ae174760f7
OliverCotton <oliver.i.cotton@hotmail.com>
2022-05-03 16:58:42 +0100

    gets first name of product from site

--- scraper.py
+++ scraper.py
@@ -7,36 +7,37 @@ import time
 
 class Scraper:
         
+        time.sleep(2)
+        print("Scraper ready")
+        driver = webdriver.Firefox()
+        URL = "https://www.ethicalsuperstore.com/special-offers/"
+        driver.get(URL)
+        driver.implicitly_wait(10)
+
+        def __init__(self):
+            
+            self.cookie_clicker()
+        
         def cookie_clicker(self):
 
             try:
-                cookie_button = WebDriverWait(self.driver, 20).until(
-                EC.element_to_be_clickable((By.XPATH, '"//*[@id="onetrust-accept-btn-handler"')))
-
-             #cookie_button = driver.find_element_by_name(by=By.XPATH, value = '//*[@id="onetrust-accept-btn-handler"]')
-                cookie_button.click
+                cookie_button = self.driver.find_element(by=By.ID, value = "onetrust-accept-btn-handler")
+                cookie_button.click()
                 print("cookies found")
             except:
                 print("no cookies")
                 pass
         
-        def open(self):
-            
-            time.sleep(2)
-            print("Scraper ready")
-            driver = webdriver.Firefox()
-            URL = "https://www.ethicalsuperstore.com/special-offers/"
-            driver.get(URL)
-            driver.implicitly_wait(10)
-            self.cookie_clicker(self)
+        
 
         
 
 
 @dataclass
 class product():
-    scraper = Scraper
-    scraper.open(scraper)
+    
+    scraper = Scraper()
+    
     def product_name(self) -> str:
         """Opens driver and goes to page"""
         product_link =  self.scraper.driver.find_element(by=By.XPATH, value ='/html/body/div[1]/div/div/div[2]/div/div/div[2]/div[3]/div[1]/div[4]/div[1]/article/div/h2/a')
@@ -44,7 +45,6 @@ class product():
         return product_linktext
 
 def main():
-    scraper = Scraper
     product1 = product
     
     print(f"product 1 is called: {product1.product_name(product1)}")
